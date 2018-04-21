76cb5df9ad8839e66d4c10bbe75ae36101d39b9e
Master456 <hendrik.frueh@gmail.com>
2018-04-21 12:46:55 +0200

    Moving to v2.0

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-#!bin/bash
 input () {
   echo "Getting settings ..."
+  read -p "Do You want to disable IPv6 globaly? (y/n) " ipv6
   read -p "What IP-adress should be assigned to this Raspberry Pi? " ip
   read -p "What IP-adress should be set as gateway? " gateway
   read -p "What SSID should be assigned to the hotspot? " ssid
@@ -10,14 +10,31 @@ input () {
   read -p "What name should be assigned to the Spotify-Connect server? " name
   read -p "Do You want to set an USB-soundcard as output? (y/n) " audio
   read -p "Do you want to set an initial volume? (y/n)" volumeyn
-
 }
 
 run () {
+  step1
+  step2
   step3
   step4
   step5
   finalize
+}
+
+step1 () {
+  echo "Step 1 of 5: Installing updates ..."
+  if [ "$ipv6" = "y" ]
+    then sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf && sudo sysctl -p
+  fi
+  sudo apt update
+  sudo apt -y full-upgrade
+}
+
+step2 () {
+  echo "Step 2 of 5: Installing the needed programms..."
+  sudo apt -y install hostapd bridge-utils git
+  echo "Getting additional files ..."
+  git clone https://github.com/Master456/RasPi-Einrichtung.git /home/pi/RasPi-Einrichtung
 }
 
 step3 () {
@@ -51,7 +68,7 @@ step5 () {
   fi
   if [ "$volumeyn" = "y" ]
     then {
-      read -p "How high should the volume be? (0-100) " volume
+      read -p "How high should the initial volume be? (0-100) " volume
       sudo echo "VOLUME_ARGS="--enable-volume-normalisation --initial-volume "$volume""" >> /etc/default/raspotify
     }
   fi
